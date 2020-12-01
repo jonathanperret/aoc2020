1f7e40e3e57286089c1fda12c5441a371368e0db
Jonathan Perret <j-github@jonathanperret.net>
2020-12-01 06:10:39 +0100

    day1a

---
+++ day1/cli/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace cli
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("day1.txt");
+            int[] amounts = lines.Select(l => int.Parse(l)).ToArray();
+            for (int i = 0; i < amounts.Length - 1; i++)
+            {
+                for (int j = i + 1; j < amounts.Length; j++)
+                {
+                    if (amounts[i] + amounts[j] == 2020)
+                    {
+                        Console.WriteLine(amounts[i] * amounts[j]);
+                    }
+
+                }
+            }
+        }
+    }
+}
---
+++ day1/tests/UnitTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using cli;
+
+namespace tests
+{
+    public class Day1Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Day1Test1()
+        {
+            //            Assert.AreEqual(966, cli.Program.ComputeFuel(1969));
+        }
+
+    }
+}
