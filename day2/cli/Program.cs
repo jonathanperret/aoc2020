edd70f2972d71d3f8f4a0f3996a3bfa389f77088
Jonathan Perret <j-github@jonathanperret.net>
2020-12-02 06:08:06 +0100

    day2a

---
+++ day2/cli/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using MoreLinq;
+
+namespace cli
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("day2.txt");
+
+            int validCount = 0;
+            foreach (string line in lines)
+            {
+                var sp = line.Split(" ");
+                var minmax = sp[0].Split("-").Select(int.Parse).ToArray();
+                int min = minmax[0], max = minmax[1];
+                char letter = sp[1].Substring(0, 1).ToCharArray()[0];
+                string pass = sp[2];
+                int letterCount = pass.Where(c => c == letter).Count();
+                bool valid = letterCount >= min && letterCount <= max;
+                if (valid) validCount++;
+                Console.WriteLine($"{min} {max} {letter} {pass} {letterCount} {valid}");
+            }
+
+            Console.WriteLine(validCount);
+        }
+    }
+}
---
+++ day2/tests/UnitTests.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using cli;
+
+namespace tests
+{
+    public class Day2Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Day2Test1()
+        {
+            //Assert.AreEqual(966, cli.Program.ComputeFuel(1969));
+        }
+
+    }
+}
