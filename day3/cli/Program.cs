af19685ff802d47b29d302465daa3c499609ab88
Jonathan Perret <j-github@jonathanperret.net>
2020-12-03 07:27:20 +0100

    linq-ify day3

--- day3/cli/Program.cs
+++ day3/cli/Program.cs
@@ -9,39 +9,25 @@ namespace cli
 {
     public static class Program
     {
-        public static Parser<(int, int, char, string)> Line =
-            (from min in Parse.Number.Select(int.Parse)
-             from dash in Parse.Char('-')
-             from max in Parse.Number.Select(int.Parse).Token()
-             from letter in Parse.Letter.Token()
-             from colon in Parse.Char(':')
-             from password in Parse.Letter.Many().Text().Token()
-             select (min, max, letter, password)).End();
+        public static long Product(this IEnumerable<long> seq)
+        {
+            return seq.Aggregate((a, b) => a * b);
+        }
 
         static void Main(string[] args)
         {
-            var lines = File.ReadAllLines("day3.txt");
-            bool[][] trees = lines.Select(line => line.Select(c => c == '#').ToArray()).ToArray();
-            int width = trees[0].Length;
-            int height = trees.Length;
+            string[] lines = File.ReadAllLines("day3.txt");
 
-            long total = 1;
-            foreach (var (sx, sy) in new (int, int)[] { (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) })
-            {
-                int col = 0;
-                long encountered = 0;
-                for (int r = 0; r < height; r += sy)
-                {
-                    if (trees[r][col])
-                        encountered++;
+            (int x, int y)[] slopes = { (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) };
 
-                    col = (col + sx) % width;
-                }
-                Console.WriteLine($"{encountered}");
-                total *= encountered;
-            }
+            long total = lines.Select((line, y) =>
+                    slopes.Select(s => y % s.y == 0 && line[s.x * y / s.y % line.Length] == '#' ? 1L : 0)
+                )
+                .Transpose()
+                .Select(s => s.Sum())
+                .Product();
 
-            Console.WriteLine($"{total}");
+            Console.WriteLine(total);
         }
     }
 }
