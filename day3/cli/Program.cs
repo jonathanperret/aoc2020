e70f6550cfffacc32f00a292699783b4b41f40d9
Jonathan Perret <j-github@jonathanperret.net>
2020-12-03 08:33:46 +0100

    Experiment with a "Matrix" Linq extension

--- day3/cli/Program.cs
+++ day3/cli/Program.cs
@@ -14,18 +14,29 @@ namespace cli
             return seq.Aggregate((a, b) => a * b);
         }
 
+        public static IEnumerable<IEnumerable<TResult>> Matrix<TRow, TColumn, TResult>(this IEnumerable<TRow> rows, IEnumerable<TColumn> columns, Func<TRow, TColumn, TResult> fn)
+        {
+            return rows.Select((row) => columns.Select(column => fn(row, column)));
+        }
+
+        public static IEnumerable<IEnumerable<TResult>> Matrix<TRow, TColumn, TResult>(this IEnumerable<TRow> rows, IEnumerable<TColumn> columns, Func<TRow, TColumn, int, int, TResult> fn)
+        {
+            return rows.Select((row, rowIndex) => columns.Select((column, columnIndex) => fn(row, column, rowIndex, columnIndex)));
+        }
+
         static void Main(string[] args)
         {
             string[] lines = File.ReadAllLines("day3.txt");
 
             (int x, int y)[] slopes = { (1, 1), (3, 1), (5, 1), (7, 1), (1, 2) };
 
-            long total = lines.Select((line, y) =>
-                    slopes.Select(s => y % s.y == 0 && line[s.x * y / s.y % line.Length] == '#' ? 1L : 0)
-                )
-                .Transpose()
-                .Select(s => s.Sum())
-                .Product();
+            long total =
+                        lines.Matrix(slopes,
+                              (line, s, y, x) => y % s.y == 0 && line[s.x * y / s.y % line.Length] == '#' ? 1L : 0
+                        )
+                            .Transpose()
+                            .Select(s => s.Sum())
+                            .Product();
 
             Console.WriteLine(total);
         }
