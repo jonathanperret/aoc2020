0aef8054dffa649f98cfee3176842650c0cc6f10
Jonathan Perret <j-github@jonathanperret.net>
2020-12-04 06:09:13 +0100

    day4a

--- day4/cli/Program.cs
+++ day4/cli/Program.cs
@@ -14,17 +14,28 @@ namespace cli
         {
             var lines = File.ReadAllLines("day4.txt");
 
+            string[] required = {
+                "byr",
+                "iyr",
+                "eyr",
+                "hgt",
+                "hcl",
+                "ecl",
+                "pid",
+             };
+
+            int result = lines.Segment(line => string.IsNullOrEmpty(line)).Select(ls => ls.ToDelimitedString(" "))
+            .Select(p => p.Trim().Split(" ").Select(kv => kv.Split(":")[0]).Intersect(required))
+            .Where(fields => fields.Count() == required.Count())
+            .Count();
+            // .ForEach(v => Console.WriteLine(v.ToDelimitedString(",")));
+
             // var (min, max) = File.ReadAllText("day4.txt").Split("-").Select(int.Parse);
-            // int result = MoreEnumerable.Sequence(min, max).Select(x=>x.ToString()).Where(x =>
-            // {
-            //     string s = x.ToString();
+            // int result = MoreEnumerable.Sequence(min, max).Select(x => x.ToString()).Where(s =>
+            //           s.Pairwise((c1, c2) => c2 >= c1).All(x => x) &&
+            //           s.GroupAdjacent(c => c).Count(g => g.Count() == 2) >= 1
+            // ).Count();
 
-            //     return
-            //         s.Pairwise((c1, c2) => c2 >= c1).All(x => x) &&
-            //         s.GroupAdjacent(c => c).Count(g => g.Count() == 2) >= 1;
-            // }).Count();
-
-            int result = 0;
 
             Console.WriteLine($"{result}");
         }
