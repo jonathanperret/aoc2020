f75f7d8fc7a824e541edb6a2086ccbe07279dae6
Jonathan Perret <j-github@jonathanperret.net>
2020-12-04 06:55:27 +0100

    Regex-based checks

--- day4/cli/Program.cs
+++ day4/cli/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using Sprache;
 using System.Numerics;
 using MoreLinq;
+using System.Text.RegularExpressions;
 
 namespace cli
 {
@@ -23,50 +24,41 @@ namespace cli
                 "ecl",
                 "pid",
              };
+
+            int x;
+
+            (string pattern, Func<string[], bool> check)[] rules = {
+                ("^byr:([0-9]{4})$", cs => int.TryParse(cs[0], out x) && x >= 1920 && x <= 2002), // (Birth Year) - four digits; at least 1920 and at most 2002.
+                ("^iyr:([0-9]{4})$", cs => int.TryParse(cs[0], out x) && x >= 2010 && x <= 2020), // (Issue Year) - four digits; at least 2010 and at most 2020.
+                ("^eyr:([0-9]{4})$", cs => int.TryParse(cs[0], out x) && x >= 2020 && x <= 2030), // (Expiration Year) - four digits; at least 2020 and at most 2030.
+                ("^hgt:([0-9]+)(cm|in)$", cs => int.TryParse(cs[0], out x) && x >= (cs[1]=="cm" ? 150:59) && x <= (cs[1]=="cm" ? 193:76)), // (Height) - a number followed by either cm or in:
+                                //cm, the number must be at least 150 and at most 193.
+                                //in, the number must be at least 59 and at most 76.
+                ("^hcl:(#[0-9a-f]{6})$", _ => true), // (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+                ("^ecl:(amb|blu|brn|gry|grn|hzl|oth)$", _ => true), // (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+                ("^pid:([0-9]{9})$", _ => true), // (Passport ID) - a nine-digit number, including leading zeroes.
+                ("^cid:.*$", _ => true), // (Country ID) - ignored, missing or not.
+            };
             int result = lines.Segment(line => string.IsNullOrEmpty(line)).Select(ls => ls.ToDelimitedString(" "))
             .Select(p => p.Trim().Split(" "))
             .Where(kvs => kvs.Select(kv => kv.Split(":")[0]).Intersect(required).Count() == required.Count())
-            .Where(kvs => kvs.All(kv =>
+            .Where(kvs => kvs.All(kv => rules.Any(rule =>
             {
-                var (k, v) = (kv.Split(":"));
-
-                int x;
-                switch (k)
+                var m = Regex.Match(kv, rule.pattern);
+                if (!m.Success)
                 {
-                    case "byr": // (Birth Year) - four digits; at least 1920 and at most 2002.
-                        return int.TryParse(v, out x) && x >= 1920 && x <= 2002;
-                    case "iyr": // (Issue Year) - four digits; at least 2010 and at most 2020.
-                        return int.TryParse(v, out x) && x >= 2010 && x <= 2020;
-                    case "eyr": // (Expiration Year) - four digits; at least 2020 and at most 2030.
-                        return int.TryParse(v, out x) && x >= 2020 && x <= 2030;
-                    case "hgt": // (Height) - a number followed by either cm or in:
-                                //cm, the number must be at least 150 and at most 193.
-                                //in, the number must be at least 59 and at most 76.
-                        var (num, unit) = v.Partition(c => char.IsDigit(c));
-                        switch (unit.ToDelimitedString(""))
-                        {
-                            case "cm":
-                                return int.TryParse(num.ToDelimitedString(""), out x) && x >= 150 && x <= 193;
-                            case "in":
-                                return int.TryParse(num.ToDelimitedString(""), out x) && x >= 59 && x <= 76;
-                            default:
-                                return false;
-                        }
-                    case "hcl": // (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-                        return v[0] == '#' && v.Length == 7 && v.Substring(1).All(c => char.IsDigit(c) || (c >= 'a' && c <= 'f'));
-                    case "ecl": // (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-                        return v == "amb" || v == "blu" || v == "brn" || v == "gry" || v == "grn" || v == "hzl" || v == "oth";
-                    case "pid": // (Passport ID) - a nine-digit number, including leading zeroes.
-                        return v.Length == 9 && v.All(c => char.IsDigit(c));
-                    case "cid": // (Country ID) - ignored, missing or not.
-                        return true;
-                    default:
-                        return false;
+                    Console.WriteLine($"{kv} fails regex {rule.pattern}");
+                    return false;
                 }
-            }))
+                bool checkd = rule.check(m.Groups.Values.Skip(1).Select(g => g.Value).ToArray());
+                if (!checkd)
+                {
+                    Console.WriteLine($"{kv} fails check");
+                }
+                return checkd;
+            })))
             .Pipe(kvs => Console.WriteLine(kvs.OrderBy(kv => kv).ToDelimitedString("\t")))
             .Count();
-            // .ForEach(v => Console.WriteLine(v.ToDelimitedString(",")));
 
             Console.WriteLine($"{result}");
         }
