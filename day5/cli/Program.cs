8a250b99f40eeddb3aa08556d2a0b98911aa341d
Jonathan Perret <j-github@jonathanperret.net>
2020-12-05 06:22:01 +0100

    day5b

---
+++ day5/cli/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Sprache;
+using System.Numerics;
+//using MoreLinq;
+using System.Text.RegularExpressions;
+
+namespace cli
+{
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("day5.txt");
+
+            var seats = lines.Select(line => line.Replace("F", "0").Replace("B", "1").Replace("R", "1").Replace("L", "0"))
+            .Select(bin => Convert.ToInt64(bin, 2))
+            .ToHashSet();
+
+            long result = Enumerable.Range(0, 1000).Where(x => !seats.Contains(x) && seats.Contains(x - 1) && seats.Contains(x + 1)).First();
+
+            Console.WriteLine($"{result}");
+        }
+    }
+}
---
+++ day5/tests/UnitTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using cli;
+using Sprache;
+
+namespace tests
+{
+    public class Day5Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Day5Test1()
+        {
+            // Assert.AreEqual((1, 2, 'a', "baba"), Program.Line.Parse("1-2 a: baba"));
+        }
+
+    }
+}
