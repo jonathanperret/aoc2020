a22c83c205f4a051fd1ffffbc1b00614b7d79973
Jonathan Perret <j-github@jonathanperret.net>
2020-12-06 06:10:57 +0100

    day6b

---
+++ day6/cli/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Sprache;
+using System.Numerics;
+using MoreLinq;
+using System.Text.RegularExpressions;
+
+namespace cli
+{
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines("day6.txt");
+            var groups = lines.Split("");
+
+            var questions = groups.Select(g => g.Aggregate((s1, s2) => s1.Distinct().Intersect(s2.Distinct()).ToDelimitedString("")).Count()).Sum();
+            Console.WriteLine(questions);
+        }
+    }
+}
