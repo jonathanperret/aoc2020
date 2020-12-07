4c504f5df8ad69013606a0697c2fd80151a98bd5
Jonathan Perret <j-github@jonathanperret.net>
2020-12-07 06:32:19 +0100

    day7b

--- day7/cli/Program.cs
+++ day7/cli/Program.cs
@@ -25,29 +25,53 @@ namespace cli
 
             string[] goodtypes = { mybag };
 
-            while (true)
-            {
-                int current = goodtypes.Length;
-                var newtypes = lines.Where(line => goodtypes.Any(t => line.Contains(" " + t + " ")))
-                .Select(line => line.Split(" bags contain ")[0]).ToArray();
-                W(newtypes.Length);
-                goodtypes = goodtypes.Concat(newtypes).Distinct().ToArray();
-                W(goodtypes.Length);
-                if (goodtypes.Length <= current) break;
-            }
-
-            W(goodtypes.OrderBy(x => x).Str("\n"));
-
-            W(goodtypes.Length - 1);
-
-
-            // lines.Select(line =>
+            // while (true)
             // {
-            //     var (type, contents) = line.Split(" bags contain ");
-            //     contents = contents.Replace(" bag, ", ", ").Replace(" bags, ", ", ").Replace(" bags.", "").Replace(" bag.", ""); ;
-            //     W(contents);
-            //     return type;
-            // }).Count();
+            //     int current = goodtypes.Length;
+            //     var newtypes = lines.Where(line => goodtypes.Any(t => line.Contains(" " + t + " ")))
+            //     .Select(line => line.Split(" bags contain ")[0]).ToArray();
+            //     W(newtypes.Length);
+            //     goodtypes = goodtypes.Concat(newtypes).Distinct().ToArray();
+            //     W(goodtypes.Length);
+            //     if (goodtypes.Length <= current) break;
+            // }
+
+            // W(goodtypes.OrderBy(x => x).Str("\n"));
+
+            // W(goodtypes.Length - 1);
+
+
+            var tofill = new List<(int count, string type)>() { (1, mybag) };
+            int tobuy = 0;
+
+            while (tofill.Count > 0)
+            {
+                int current = tobuy;
+                foreach (string line in lines)
+                {
+                    var (type, contents) = line.Split(" bags contain ");
+                    contents = contents.Replace(" bag, ", ",").Replace(" bags, ", ",").Replace(" bags.", "").Replace(" bag.", "");
+                    if (tofill.Any(pair => pair.type == type))
+                    {
+                        W($"filling {type}");
+                        var (count, _) = tofill.Find(pair => pair.type == type);
+                        tofill.Remove((count, type));
+                        W($"buying {count} of {type}");
+                        tobuy += count;
+                        foreach (string contained in contents.Split(","))
+                        {
+                            if (contained == "no other") continue;
+                            W($"parsing {contained}");
+                            var words = contained.Split(" ");
+                            var (containedCount, containedType) = (int.Parse(words[0]), words[1] + " " + words[2]);
+                            W($"adding {containedCount}*{count} of {containedType}");
+                            tofill.Add((containedCount * count, containedType));
+                        }
+                    }
+                }
+            }
+            W($"bought {tobuy - 1} bags");
+
         }
     }
 }
