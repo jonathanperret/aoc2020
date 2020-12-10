6549d92a3125561307c46f3d085bcb7958b67141
Jonathan Perret <j-github@jonathanperret.net>
2020-12-10 06:25:15 +0100

    day10b

---
+++ day10/cli/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Sprache;
+using System.Numerics;
+using MoreLinq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+
+namespace cli
+{
+    public static class Program
+    {
+        public static void W<T>(T data)
+        {
+            Console.WriteLine(data);
+        }
+        static void Main(string[] args)
+        {
+            var text = File.ReadAllText("input.txt");
+            var lines = File.ReadAllLines("input.txt");
+            var groups = lines.Split("");
+            var levels = lines.Select(int.Parse).OrderBy(x => x).ToArray();
+            var blocks = levels.Window(26);
+            int maxlevel = levels.Max();
+            int devicelevel = maxlevel + 3;
+
+            var waysToReach = new long[devicelevel];
+            waysToReach[0] = 1;
+            foreach (int level in levels)
+            {
+                if (level >= 1) waysToReach[level] += waysToReach[level - 1];
+                if (level >= 2) waysToReach[level] += waysToReach[level - 2];
+                if (level >= 3) waysToReach[level] += waysToReach[level - 3];
+                W($"{level} : {waysToReach[level]}");
+            }
+
+            long result = waysToReach[devicelevel - 1]
+                + waysToReach[devicelevel - 2]
+                + waysToReach[devicelevel - 3];
+            W($"{devicelevel}: {result}");
+        }
+    }
+}
---
+++ day10/tests/UnitTests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+using cli;
+using Sprache;
+
+namespace tests
+{
+    public class UnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Assert.AreEqual((1, 2, 'a', "baba"), Program.Line.Parse("1-2 a: baba"));
+        }
+
+    }
+}
