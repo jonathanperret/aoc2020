156bcee1287a4cc38d521c9317b7da68c9b9b242
Jonathan Perret <j-github@jonathanperret.net>
2020-12-11 21:42:12 +0100

    before day12

---
+++ day12/cli/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Sprache;
+using System.Numerics;
+using MoreLinq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using static Util;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        var text = File.ReadAllText("input.txt");
+        var lines = File.ReadAllLines("input.txt");
+        var groups = lines.Split("");
+        var numbers = lines.Select(int.Parse).ToArray();
+        var blocks = numbers.Window(26);
+        int max = numbers.Max();
+        int min = numbers.Min();
+        var matrix = lines.Select(
+            line => line.ToCharArray()
+        ).ToArray();
+
+        int result = max - min;
+        W($"{result}");
+    }
+}
