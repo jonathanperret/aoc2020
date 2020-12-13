ae810610abf3e95bf6a0eb8d624ab3f256ad4c5f
Jonathan Perret <j-github@jonathanperret.net>
2020-12-13 20:56:34 +0100

    day14 before

---
+++ day14/tests/UnitTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace tests
+{
+    public class UnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Assert.AreEqual((1, 2, 'a', "baba"), Program.Line.Parse("1-2 a: baba"));
+        }
+
+    }
+}
