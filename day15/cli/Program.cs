7d6f046c27656ef12acd323ec4aaca62930a116d
Jonathan Perret <j-github@jonathanperret.net>
2020-12-14 22:31:21 +0100

    day15 before

---
+++ day15/cli/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Sprache;
+using System.Numerics;
+using MoreLinq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using static Util;
+
+public static class Program
+{
+    public static int Solve(string[] lines)
+    {
+        return lines.Length;
+    }
+
+    static void Main(string[] args)
+    {
+        var text = File.ReadAllText("input.txt");
+        var lines = File.ReadAllLines("input.txt");
+        var groups = lines.Split("");
+        // var numbers = lines.Select(int.Parse).ToArray();
+        // var blocks = numbers.Window(26);
+        // int max = numbers.Max();
+        // int min = numbers.Min();
+
+        var result = Solve(lines);
+        W($"{result}");
+    }
+}
---
+++ day15/tests/UnitTests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using static Program;
+using System.IO;
+
+namespace tests
+{
+    public class UnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(11, Solve(
+                File.ReadAllLines("../../../../cli/example.txt")
+            ));
+        }
+    }
+}
