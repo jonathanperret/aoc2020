ee0f34b7d9c15ac7f4d3cb00a33660ba96b6a786
Jonathan Perret <j-github@jonathanperret.net>
2020-12-15 06:24:08 +0100

    day15b

--- day15/cli/Program.cs
+++ day15/cli/Program.cs
@@ -10,22 +10,44 @@ using static Util;
 
 public static class Program
 {
-    public static int Solve(string[] lines)
+    public static int Solve(int[] numbers, int count = 2020)
     {
-        return lines.Length;
+        int last = 0;
+        var lastpos = new Dictionary<int, int>();
+        var previouspos = new Dictionary<int, int>();
+        var ages = new Dictionary<int, int>();
+        for (int i = 0; i < count; i++)
+        {
+            if (i < numbers.Length)
+            {
+                last = numbers[i];
+            }
+            else
+            {
+                if (!previouspos.ContainsKey(last))
+                {
+                    last = 0;
+                }
+                else
+                {
+                    last = lastpos[last] - previouspos[last];
+                }
+
+            }
+            if (lastpos.ContainsKey(last))
+                previouspos[last] = lastpos[last];
+            lastpos[last] = i;
+            W(last);
+        }
+        return last;
     }
 
     static void Main(string[] args)
     {
         var text = File.ReadAllText("input.txt");
-        var lines = File.ReadAllLines("input.txt");
-        var groups = lines.Split("");
-        // var numbers = lines.Select(int.Parse).ToArray();
-        // var blocks = numbers.Window(26);
-        // int max = numbers.Max();
-        // int min = numbers.Min();
-
-        var result = Solve(lines);
+        // var text = File.ReadAllText("example.txt");
+        var numbers = text.Split(",").Select(int.Parse).ToArray();
+        var result = Solve(numbers, 30000000);
         W($"{result}");
     }
 }
--- day15/tests/UnitTests.cs
+++ day15/tests/UnitTests.cs
@@ -14,9 +14,18 @@ namespace tests
         [Test]
         public void Test1()
         {
-            Assert.AreEqual(11, Solve(
-                File.ReadAllLines("../../../../cli/example.txt")
-            ));
+            Assert.AreEqual(1, Solve(new int[] { 1, 3, 2 }));
+            Assert.AreEqual(10, Solve(new int[] { 2, 1, 3 }));
+            Assert.AreEqual(27, Solve(new int[] { 1, 2, 3 }));
+            Assert.AreEqual(78, Solve(new int[] { 2, 3, 1 }));
+            Assert.AreEqual(438, Solve(new int[] { 3, 2, 1 }));
+            Assert.AreEqual(1836, Solve(new int[] { 3, 1, 2 }));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(2578, Solve(new int[] { 1, 3, 2 }, 30000000));
         }
     }
 }
