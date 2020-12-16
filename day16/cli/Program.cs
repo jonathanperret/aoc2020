fff268e253021f8683fab641e052fb279dbafdaa
Jonathan Perret <j-github@jonathanperret.net>
2020-12-16 06:13:46 +0100

    day16a

---
+++ day16/cli/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Sprache;
+using System.Numerics;
+using MoreLinq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using static Util;
+
+public static class Program
+{
+    public static int Solve(string[] lines)
+    {
+        var groups = lines.Split("").ToArray();
+
+        var fields = groups[0].SelectMany(fieldstr =>
+        {
+            return fieldstr.Split(": ")[1].Split(" or ")
+                .Select(rangestr => rangestr.Split("-").Select(int.Parse).Fold((min, max) => (min: min, max: max))
+                ).ToArray();
+        }).ToArray();
+
+        W(fields.ToDelimitedString(","));
+
+        var tickets = groups[2].Skip(1).Select(
+            ticketstr => ticketstr.Split(",").Select(int.Parse).ToArray()
+        ).ToArray();
+
+        W(tickets.Select(t => "(" + t.ToDelimitedString(",") + ")").ToDelimitedString(","));
+
+        var invalidSum = tickets.SelectMany(values =>
+            values.Where(value => !fields.Any(field =>
+                value >= field.min && value <= field.max
+            ))
+        ).Sum();
+
+        return invalidSum;
+    }
+
+    static void Main(string[] args)
+    {
+        var text = File.ReadAllText("input.txt");
+        var lines = File.ReadAllLines("input.txt");
+        // var numbers = lines.Select(int.Parse).ToArray();
+        // var blocks = numbers.Window(26);
+        // int max = numbers.Max();
+        // int min = numbers.Min();
+
+        var result = Solve(lines);
+        W($"{result}");
+    }
+}
---
+++ day16/tests/UnitTests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using static Program;
+using System.IO;
+
+namespace tests
+{
+    public class UnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(71, Solve(
+                File.ReadAllLines("../../../../cli/example.txt")
+            ));
+        }
+    }
+}
