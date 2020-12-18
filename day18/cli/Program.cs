966b1225d7e2325eaa29783c95cad8cd086e15ac
Jonathan Perret <j-github@jonathanperret.net>
2020-12-18 09:36:37 +0100

    day18 with Sprache

--- day18/cli/Program.cs
+++ day18/cli/Program.cs
@@ -2,52 +2,26 @@
 using System.IO;
 using System.Linq;
 using Sprache;
-using System.Numerics;
-using MoreLinq;
-using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using static Util;
-using System.Diagnostics;
 
 public static class Program
 {
+    static Parser<char> Add = Parse.Char('+').Token();
+    static Parser<char> Multiply = Parse.Char('*').Token();
+    static Parser<long> Constant = Parse.Number.Select(long.Parse);
+
+    static Parser<long> Factor =
+        (from lparen in Parse.Char('(')
+         from expr in Product
+         from rparen in Parse.Char(')')
+         select expr)
+         .XOr(Constant);
+
+    static Parser<long> Sum = Parse.ChainOperator(Add, Factor, (_op, a, b) => a + b);
+    static Parser<long> Product = Parse.ChainOperator(Multiply, Sum, (_op, a, b) => a * b);
+
     public static long Compute(string expr)
     {
-        var states = new Stack<(long sum, long product, char op)>();
-        char op = '+';
-        long sum = 0;
-        long product = 1;
-        foreach (char c in expr.Replace(" ", ""))
-        {
-            switch (c)
-            {
-                default:
-                    var value = long.Parse(c.ToString());
-                    if (op == '+') sum += value; else product = value;
-                    break;
-                case '*':
-                    product *= sum;
-                    sum = 0;
-                    op = '+';
-                    break;
-                case '+':
-                    op = c;
-                    break;
-                case '(':
-                    states.Push((sum, product, op));
-                    op = '+';
-                    sum = 0;
-                    product = 1;
-                    break;
-                case ')':
-                    value = product * sum;
-                    (sum, product, op) = states.Pop();
-                    if (op == '+') sum += value; else product = value;
-                    break;
-            }
-
-        }
-        return product * sum;
+        return Product.Parse(expr);
     }
     public static long Solve(string[] lines)
     {
@@ -56,15 +30,6 @@ public static class Program
 
     static void Main(string[] args)
     {
-        var text = File.ReadAllText("input.txt");
-        var lines = File.ReadAllLines("input.txt");
-        var groups = lines.Split("");
-        // var numbers = lines.Select(int.Parse).ToArray();
-        // var blocks = numbers.Window(26);
-        // int max = numbers.Max();
-        // int min = numbers.Min();
-
-        var result = Solve(lines);
-        W($"{result}");
+        Console.WriteLine(Solve(File.ReadAllLines("input.txt")));
     }
 }
