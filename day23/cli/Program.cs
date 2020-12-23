e6929788159a52eacf3f781339b1dedf0e149a42
Jonathan Perret <j-github@jonathanperret.net>
2020-12-23 07:25:00 +0100

    day22 part1 with array

--- day23/cli/Program.cs
+++ day23/cli/Program.cs
@@ -10,34 +10,53 @@ using static Util;
 
 public static class Program
 {
-    public static string Move(string cups)
+    public static void D(string s)
     {
-        char currentCup = cups[0];
-        string picked = cups[1..4];
-        W($"current {currentCup} picked {picked}");
-        string remaining = cups[4..];
-        W($"remaining {remaining}");
-        char dest = currentCup;
-        int destpos = -1;
-        int t = 0;
-        while (destpos < 0 && t++ < 11)
-        {
-            dest = dest == '1' ? '9' : (char)(dest - 1);
-            destpos = remaining.IndexOf(dest);
-        }
-        W($"dest={dest} destpos={destpos} t={t}");
-        return $"{remaining[0..(destpos + 1)]}{picked}{remaining[(destpos + 1)..]}{currentCup}";
+        // W(s);
     }
 
-    public static string After1(string cups)
+    public static int maxVal = 9;
+    public static int[] Move(int[] cups)
     {
-        // fake
-        return cups[1..];
+        int currentCup = cups[0];
+        int[] picked = cups[1..4];
+        D($"current {currentCup} picked {picked.ToDelimitedString(",")}");
+        D($"remaining {cups[4..].ToDelimitedString(",")}");
+        int dest = currentCup;
+        int destpos = -1;
+        int t = 0;
+        while (destpos < 0 && t++ < 2 * maxVal)
+        {
+            dest = dest == 1 ? maxVal : (char)(dest - 1);
+            destpos = Array.IndexOf(cups, dest, 4) - 4;
+        }
+        D($"dest={dest} destpos={destpos} t={t}");
+        int[] result = new int[cups.Length];
+        result[^1] = currentCup;
+        Array.Copy(cups, 4, result, 0, destpos + 1);
+        Array.Copy(picked, 0, result, destpos + 1, 3);
+        Array.Copy(cups, 4 + destpos + 1, result, destpos + 4, cups.Length - 4 - destpos - 1);
+        D($"result={result.ToDelimitedString(",")}");
+        return result;
+    }
+
+    public static string After1(int[] cups)
+    {
+        int onePos = Array.IndexOf(cups, 1);
+        int[] result = new int[cups.Length - 1];
+        int rightLen = cups.Length - onePos - 1;
+        int leftLen = onePos;
+        if (rightLen > 0)
+            Array.Copy(cups, (onePos + 1) % cups.Length, result, 0, rightLen);
+        if (leftLen > 0)
+            Array.Copy(cups, 0, result, rightLen, leftLen);
+        return result.ToDelimitedString("");
     }
 
     public static string Part1(string sequence)
     {
-        var cups = sequence.Trim();
+        var cups = sequence.Trim().Select(c => (int)c - '0').ToArray();
+        maxVal = 9;
 
         return After1(MoreEnumerable.Generate(cups, Move).ElementAt(100));
     }
--- day23/tests/UnitTests.cs
+++ day23/tests/UnitTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using static Program;
 using System.IO;
+using System.Linq;
 
 namespace tests
 {
@@ -20,15 +21,28 @@ namespace tests
         }
 
         [Test]
+        public void TestAfter1()
+        {
+            Assert.AreEqual("45678923", After1(new[] { 2, 3, 1, 4, 5, 6, 7, 8, 9 }));
+            Assert.AreEqual("23456789", After1(new[] { 2, 3, 4, 5, 6, 7, 8, 9, 1 }));
+        }
+
+        [Test]
         public void TestMove()
         {
-            Assert.AreEqual("289154673", Move("389125467"));
+            CollectionAssert.AreEqual(
+                new[] { 2, 8, 9, 1, 5, 4, 6, 7, 3 },
+                Move(new[] { 3, 8, 9, 1, 2, 5, 4, 6, 7 })
+            );
         }
 
         [Test]
         public void TestMove2()
         {
-            Assert.AreEqual("936725841", Move("136792584"));
+            CollectionAssert.AreEqual(
+                new[] { 9, 3, 6, 7, 2, 5, 8, 4, 1 },
+                Move(new[] { 1, 3, 6, 7, 9, 2, 5, 8, 4 })
+            );
         }
     }
 }
