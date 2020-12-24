ba05b19c77af9e230f52c48841a6fcdc490acd1a
Jonathan Perret <j-github@jonathanperret.net>
2020-12-24 06:16:36 +0100

    day24 part 1

---
+++ day24/cli/Program.cs
@@ -0,0 +1,56 @@
+using System;
+using System.IO;
+using System.Linq;
+using Sprache;
+using System.Numerics;
+using MoreLinq;
+using System.Text.RegularExpressions;
+using System.Collections.Generic;
+using static Util;
+
+public static class Program
+{
+    public static int Part1(string[] lines)
+    {
+        var flipped = lines.Select(line =>
+        {
+            string[] dirs = Regex.Matches(line, "e|w|ne|nw|se|sw").Select(m => m.Value).ToArray();
+
+            (int, int) coord =
+                dirs.Aggregate((0, 0), (c, dir) =>
+                {
+                    var (x, y) = c;
+                    return dir switch
+                    {
+                        "e" => (x + 1, y),
+                        "w" => (x - 1, y),
+                        "ne" => (x, y + 1),
+                        "nw" => (x - 1, y + 1),
+                        "sw" => (x, y - 1),
+                        "se" => (x + 1, y - 1),
+                        _ => throw new Exception("bad dir")
+                    };
+                });
+
+            W($"{dirs.Str(",")} => {coord}");
+            return coord;
+        });
+
+        W(flipped.OrderBy(c => c).Str(","));
+
+        return flipped.GroupBy(c => c).Where(g => g.Count() % 2 == 1).Count();
+    }
+
+    static void Main(string[] args)
+    {
+        var text = File.ReadAllText("input.txt");
+        var lines = File.ReadAllLines("input.txt");
+        // var numbers = lines.Select(int.Parse).ToArray();
+        // var blocks = numbers.Window(26);
+        // int max = numbers.Max();
+        // int min = numbers.Min();
+
+        var result = Part1(lines);
+        W($"{result}");
+    }
+}
